d211e52d9bf3d1fa4152fe05a7a768206f961f34
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2024-03-04 23:29:12 +0100

    gggggg

--- Assets/ClickManager.cs
+++ Assets/ClickManager.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using TMPro;
 using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
-    private int click = 1;
+    public float click = 1;
+    public float Cash = 0;
+    public float CashPerSsec = 0;
+
+    public TextMeshProUGUI TotalCash;
+
+    private void Start()
+    {
+        StartCoroutine(IdleGain());
+    }
+    private void FixedUpdate()
+    {
+        string temp = Cash.ToString("F2");
+        TotalCash.text = temp;
+    }
+
+    public void Click()
+    {
+        Cash += click;
+    }
+
+    public void ClickPerSec()
+    {
+        CashPerSsec += 1f;
+    }
+
+    public IEnumerator IdleGain()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Cash += CashPerSsec / 10f;
+        }
+    }
 }
